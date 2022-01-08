baabe4000b3902e24c267aa8d8f81ed5c988b718
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2022-01-08 18:22:21 -0500

    Update rsbank.sol

--- contracts/rsbank.sol
+++ contracts/rsbank.sol
@@ -7,18 +7,21 @@ import "./rs20.sol";
 
 contract rsbank is ERC20 {
 
+  mapping(address => uint) public getEthBalance;
   mapping(address => uint) public entryStart;
   mapping(address => bool) public isDeposited;
 
-  event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Deposit(address indexed user, uint ethAmount, uint timeStart);
   event Withdraw(address indexed user, uint userBalance, uint depositTime, uint interest);
   event LoanTaken(address indexed user, uint Amount, uint timeStarted, uint interest);
   event LoanRepayed(address indexed user, uint timeTaken);
 
+  IERC20 public rstoken;
     
     constructor(uint256 _totalSupply) ERC20("RS 20", "RS") {
         
         _mint(msg.sender, _totalSupply);
+        rstoken = IERC20(address.this);
         
     }
 
@@ -26,8 +29,8 @@ contract rsbank is ERC20 {
     require(isDeposited[msg.sender] == false, "Error: Deposit already active!");
     require(msg.value >= 10**16, "Error: deposit value must be >= 0.01 ETH");
 
-    getEtherBalance[msg.sender] = getEtherBalance[msg.sender] + msg.value;
-    depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+    getEthBalance[msg.sender] = getEthBalance[msg.sender] + msg.value;
+    entryStart[msg.sender] = entryStart[msg.sender] + block.timestamp;
 
     isDeposited[msg.sender] = true; //activate deposit status
     emit Deposit(msg.sender, msg.value, block.timestamp);
@@ -36,26 +39,26 @@ contract rsbank is ERC20 {
   function withdraw() public {
     //check if msg.sender deposit status is true
     require(isDeposited[msg.sender] == true, 'Error: No previous deposit');
-    uint userBalance = getEtherBalance[msg.sender];
+    uint _userBalance = getEthBalance[msg.sender];
 
-    uint depositTime = block.timestamp - depositStart[msg.sender];
+    uint _depositTime = block.timestamp - entryStart[msg.sender];
     //calc accrued interest
-    uint interestPerSecond = 31668017 * (userBalance / 1e16); // 10% APY per year for 0.01 ETH
-    uint interest = interestPerSecond * depositTime;
+    uint _interestPerSecond = 31668017 * (_userBalance / 1e16); // 10% APY per year for 0.01 ETH
+    uint _interest = _interestPerSecond * _depositTime;
 
-    token.mint(msg.sender, interest);
+    rstoken.mint(msg.sender, _interest);
 
-    msg.sender.transfer(userBalance);
+    msg.sender.transfer(_userBalance);
     
-    depositStart[msg.sender] = 0;
-    getEtherBalance[msg.sender] = 0;
+    entryStart[msg.sender] = 0;
+    getEthBalance[msg.sender] = 0;
     isDeposited[msg.sender] = false;
 
-    emit Withdraw(msg.sender, userBalance, depositTime, interest);
+    emit Withdraw(msg.sender, _userBalance, entryStart, _interest);
 
   }
 
-  function borrow() payable public {
+  function borrow(uint _amount, ) payable public {
       pass;
   }
 
